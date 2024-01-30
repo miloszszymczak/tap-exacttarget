5107bdba8bc3408c9d6f49674eed6d1e5a84858e
Milosz Szymczak <szymczak.milosz@gmail.com>
2024-01-30 13:17:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ setup(
         'singer-python==5.12.1',
         'python-dateutil==2.8.2',
         'voluptuous==0.10.5',
-        'Salesforce-FuelSDK @ git+https://github.com/miloszszymczak/FuelSDK-Python.git#egg=Salesforce-FuelSDK-1.3.1',
-        'https://github.com/wbond/oscrypto/archive/d5f3437ed24257895ae1edd9e503cfb352e635a8.zip'
+        'Salesforce-FuelSDK @ git+https://github.com/miloszszymczak/FuelSDK-Python.git#egg=Salesforce-FuelSDK-1.3.1'
     ],
     extras_require={
         'test': [
